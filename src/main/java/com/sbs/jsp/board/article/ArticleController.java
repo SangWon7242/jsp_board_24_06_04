24a1ac308e77a2e2fcdd177614338ea16a40f395
SangWon7242 <gotkdqja@gmailc.com>
2024-06-14 21:24:29 +0900

    appendBody를 print로 수정

--- src/main/java/com/sbs/jsp/board/article/ArticleController.java
+++ src/main/java/com/sbs/jsp/board/article/ArticleController.java
@@ -15,7 +15,7 @@ public class ArticleController {
     List<ArticleDto> articleDtos = articleService.findAll();
 
     if (articleDtos.isEmpty()) {
-      rq.appendBody("게시물이 존재하지 않습니다.");
+      rq.print("게시물이 존재하지 않습니다.");
       return;
     }
 
@@ -44,21 +44,21 @@ public class ArticleController {
 
     long id = articleService.write(title, body);
 
-    rq.appendBody("<div>%d번 게시물이 생성되었습니다.</div>\n".formatted(id));
+    rq.print("<div>%d번 게시물이 생성되었습니다.</div>\n".formatted(id));
   }
 
   public void showDetail(Rq rq) {
     long id = rq.getLongPathValueByIndex(1, 0);
 
     if(id == 0) {
-      rq.appendBody("번호를 입력해주세요.");
+      rq.print("번호를 입력해주세요.");
       return;
     }
 
     ArticleDto articleDto = articleService.findById(id);
 
     if(articleDto == null) {
-      rq.appendBody("해당 게시물이 존재하지 않습니다.");
+      rq.print("해당 게시물이 존재하지 않습니다.");
       return;
     }
 
@@ -70,35 +70,35 @@ public class ArticleController {
     long id = rq.getLongPathValueByIndex(1, 0);
 
     if(id == 0) {
-      rq.appendBody("번호를 입력해주세요.");
+      rq.print("번호를 입력해주세요.");
       return;
     }
 
     ArticleDto articleDto = articleService.findById(id);
 
     if(articleDto == null) {
-      rq.appendBody("해당 게시물이 존재하지 않습니다.");
+      rq.print("해당 게시물이 존재하지 않습니다.");
       return;
     }
 
     articleService.delete(id);
 
-    rq.appendBody("%d번 게시물이 삭제되었습니다.".formatted(id));
-    rq.appendBody("<div><a href=\"/usr/article/list/free\">리스트로 이동</a></div>");
+    rq.print("%d번 게시물이 삭제되었습니다.".formatted(id));
+    rq.print("<div><a href=\"/usr/article/list/free\">리스트로 이동</a></div>");
   }
 
   public void showModify(Rq rq) {
     long id = rq.getLongPathValueByIndex(1, 0);
 
     if(id == 0) {
-      rq.appendBody("번호를 입력해주세요.");
+      rq.print("번호를 입력해주세요.");
       return;
     }
 
     ArticleDto articleDto = articleService.findById(id);
 
     if(articleDto == null) {
-      rq.appendBody("해당 게시물이 존재하지 않습니다.");
+      rq.print("해당 게시물이 존재하지 않습니다.");
       return;
     }
 
@@ -112,14 +112,14 @@ public class ArticleController {
     String body = rq.getParam("body", "");
 
     /*
-    rq.appendBody("<div>id : %d</div>".formatted(id));
-    rq.appendBody("<div>title : %s</div>".formatted(title));
-    rq.appendBody("<div>body %s</div>".formatted(body));
+    rq.print("<div>id : %d</div>".formatted(id));
+    rq.print("<div>title : %s</div>".formatted(title));
+    rq.print("<div>body %s</div>".formatted(body));
      */
 
     articleService.modify(id, title, body);
-    rq.appendBody("%d번 게시물이 수정되었습니다.".formatted(id));
-    rq.appendBody("""
+    rq.print("%d번 게시물이 수정되었습니다.".formatted(id));
+    rq.print("""
                   <div>
                     <a href="/usr/article/detail/free/%d">수정된 글로 이동</a>
                   </div>
